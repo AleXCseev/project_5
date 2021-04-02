569c36e43495d7f6d3946240ff89e4ca49b613e7
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-04-02 17:56:48 +0300

    add card

--- src/js/main.js
+++ src/js/main.js
@@ -5,45 +5,65 @@ $(function () {
 		loop: true,
 	});
 
-	// function modal() {
-	// 	$(".reviews .order__link").click(function () {
-	// 		$.fancybox.open($(".modal"), {
-	// 			infobar: false,
-	// 			touch: false,
-	// 		})
-	// 	})
+	$('.card__1 .slider').slick({
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+		dots: true,
+		appendDots: $(".card__1 .card__right"),
+		prevArrow: $('.card__1 .btn__prev'),
+		nextArrow: $('.card__1 .btn__next'),
+	});
+	$('.card__2 .slider').slick({
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+		dots: true,
+		appendDots: $(".card__2 .card__right"),
+		prevArrow: $('.card__2 .btn__prev'),
+		nextArrow: $('.card__2 .btn__next'),
+	});
+	$('.card__3 .slider').slick({
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+		dots: true,
+		appendDots: $(".card__3 .card__right"),
+		prevArrow: $('.card__3 .btn__prev'),
+		nextArrow: $('.card__3 .btn__next'),
+	});
 
-	// 	function readURL(input) {
-	// 		if (input.files && input.files[0]) {
-	// 			var reader = new FileReader();
-	// 			reader.onload = function (e) {
-	// 				$('.file img').attr('src', e.target.result).css("display", "block");
-	// 			};
-	// 			reader.readAsDataURL(input.files[0]);
-	// 		}
-	// 	}
+	$('.review__slider .slider').slick({
+		slidesToShow: 1,
+  		slidesToScroll: 1,
+		dots: true,
+		appendDots: $(".review__slider"),
+		prevArrow: $('.review__slider .btn__prev'),
+		nextArrow: $('.review__slider .btn__next'),
+	});
 
-	// 	$(".modal .input__file").on("change", function () {
-	// 		readURL(this);
-	// 	});
+	function modal() {
+		$(".add__review").click(function () {
+			$.fancybox.open($(".modal"), {
+				infobar: false,
+				touch: false,
+			})
+		})
 
-	// 	$(".modal form").submit(function (e) {
-	// 		e.preventDefault()
-	// 		$(this).removeClass("active");
-	// 		$(".send__window").addClass("active");
-	// 		$(".modal .name__input").val("")
-	// 		$(".modal .modal__area").val("")
-	// 		$(".modal .file img").attr("src", "").css("display", "none")
-	// 		delayClose()
-	// 	})
-	// 	function delayClose() {
-	// 		setTimeout(function () {
-	// 			$(".modal form").addClass("active");
-	// 			$(".send__window").removeClass("active");
-	// 			$(".modal .fancybox-close-small").click();
-	// 		}, 5000);
-	// 	}
-	// }
+		$(".modal form").submit(function (e) {
+			e.preventDefault()
+			$(this).removeClass("active");
+			$(".send__window").addClass("active");
+			$(".modal .name__input").val("")
+			$(".modal .modal__area").val("")
+			$(".modal .file img").attr("src", "").css("display", "none")
+			delayClose()
+		})
+		function delayClose() {
+			setTimeout(function () {
+				$(".modal form").addClass("active");
+				$(".send__window").removeClass("active");
+				$(".modal .fancybox-close-small").click();
+			}, 5000);
+		}
+	}
 
-	// modal()
+	modal()
 })
